b19fc8f05626171f312ece4fbce90a9d280c66b7
Michael Fienen <mnfienen@usgs.gov>
2013-03-16 16:51:40 -0500

    initial commit

---
+++ REIripperCONSOL.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import shapiro
+from matplotlib.backends.backend_pdf import PdfPages
+
+def resid_proc(reis,remove_zero_wt,grpfiles):
+    for cf in reis:
+        infile = reis[cf]
+        # open a pointer to the output file
+        ofp = open(infile + '_residuals_summary.dat','w')
+        ofp.write('Residuals Summary information for -> ' + infile + '\n')
+        # read in the data
+        alldat = np.genfromtxt(infile,names=True,skip_header=4,dtype=None)
+        
+        # find the unique list of groups by which plots and stats will be managed
+        allgrps = np.unique(alldat['Group'])
+        
+        # loop over the groups
+        for cg in allgrps:
+            # identify indices of the current group
+            tmpinds = np.nonzero(alldat['Group']==cg)[0]
+            if remove_zero_wt:
+                inds = tmpinds[np.nonzero(alldat['Weight'][tmpinds] != 0)]
+            else:
+                inds = tmpinds
+            # pull out the measured values for the group
+            cmeas = alldat['Measured'][inds]
+            # pull out the modeled values for the group
+            cmod =  alldat['Modelled'][inds]
+            
+            #get some values to limit plotting areas
+            cmin = np.min([cmeas,cmod])
+            cmax = np.max([cmeas,cmod])
+            
+            # make a plot of modeled vs. measured
+            plt.figure()
+            plt.hold = True
+            
+            plt.plot(cmeas,cmod,'bx')
+            plt.plot([cmin,cmax],[cmin,cmax],'r')
+            plt.title(cg + ' iteration ' + str(cf))
+            # append the histograms into the proper PDF file
+            grpfiles[cg][0].savefig()
+        
+            # now calculate statistics on the residuals
+            
+            # first grab the residuals
+            cres = alldat['Residual'][inds]
+            
+            # next calculate the relevant statistics and write to the output file
+            cmean = np.mean(cres)
+            cstd  = np.std(cres)
+            cvar  = np.var(cres)
+            cmed  = np.median(cres)
+            cmin  = np.min(cres)
+            camin = np.min(np.abs(cres))
+            cmax  = np.max(cres)
+            camax = np.max(np.abs(cres))
+            plt.close('all')
+            # finally plot the histogram and save it
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            n, bins, patches = ax.hist(cres, 50, facecolor='blue', alpha=0.75)
+            ax.set_xlabel('Residual Value')
+            ax.set_ylabel('Count')
+            ax.set_title(cg + ' iteration ' + str(cf))
+            ax.set_xlim([cmin,cmax])
+            # append the histograms into the proper PDF file
+            grpfiles[cg][1].savefig()
+            plt.close('all')
+            # perform the Shapiro-Wilks test for normality of the residuals
+            if len(cres) > 2:
+                W,p = shapiro(cres)
+            else:
+                p = -99999
+                
+            # write to the summary output file
+            ofp.write(25*'#' + '\n')
+            ofp.write('Summary Statistics for Residuals: -> group ' + cg +'\n')
+            ofp.write('%14s : %f\n' %('mean',cmean))
+            ofp.write('%14s : %f\n' %('median',cmed))
+            ofp.write('%14s : %f\n' %('std deviation',cstd))
+            ofp.write('%14s : %f\n' %('variance',cvar))
+            ofp.write('%14s : %f\n' %('min',cmin))
+            ofp.write('%14s : %f\n' %('max',cmax))
+            ofp.write('%14s : %f\n' %('min (absolute)',camin))
+            ofp.write('%14s : %f\n' %('max (absolute)',camax))
+            
+            if p > 0.05:
+                ofp.write('Residuals are not normally distributed\n')
+                ofp.write('p-value = %f' %(p))
+            elif p < -99:
+                ofp.write('Residuals normality not calculable: Too few residuals in group\n')
+            else:
+                ofp.write('Residuals are normally distributed\n')
+                ofp.write('p-value = %f' %(p))
+            
+            
+            ofp.write(3*'\n')
+        ofp.close()
+# close the PDF files
+    for cg in grpfiles:
+        for i in range(2):
+            grpfiles[cg][i].close()
---
+++ plot_reis.py
@@ -0,0 +1,47 @@
+'''
+residuals plotting for PEST
+
+This depends on resid_proc which lives in REIripper
+
+a m!ke@usgs joint
+contact: mnfienen@usgs.gov
+
+This script goes through all REI.# files in the current folder with the basecase name
+in the filename and saves a separate set of files for each iteration.
+'''
+
+import numpy as np
+import os
+import sys
+import pylab as plt
+from REIripperCONSOL import resid_proc
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+# set flag for handling weights
+remove_zero_wt = False
+# set the base case - all <basecase>.rei.# files will be processed.
+basecase = 'br_kc'
+# troll through the current directory and return a dictionary of all REI Files
+allfiles = os.listdir(os.getcwd())
+reis = dict()
+for cf in allfiles:
+    if ((basecase in cf) and ('rei' in cf[-6:]) and (cf[-4:] != '.rei')):
+        cind = int(cf.split('.')[-1])
+        reis[cind] = cf
+
+# need to get group names for the consolidated files
+alldat = np.genfromtxt(reis[reis.keys()[0]],names=True,skip_header=4,dtype=None)
+# find the unique list of groups by which plots and stats will be managed
+allgrps = np.unique(alldat['Group'])
+
+# make a dictionary to contain a PDF output file handle for each group
+grpfiles = dict()
+for cg in allgrps:
+    grpfiles[cg] = [PdfPages(basecase + "_" + cg + "_one2one.pdf"),
+                    PdfPages(basecase + "_" + cg + "_histogram.pdf")]
+  
+# doing this second loop is overkill, but consistent with the plot_bpas.py logic
+# and doesn't waste too much time   
+resid_proc(reis,remove_zero_wt,grpfiles)
+
