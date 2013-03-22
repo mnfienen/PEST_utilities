397d0b5e8cadbc569ca1abf9f9235489acb6a9e0
mnfienen <mnfienen@usgs.gov>
2013-03-22 15:16:53 -0500

    Update add2mxd.py

--- add2mxd.py
+++ add2mxd.py
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-import arcpy
-import os
-
-rootname = 'br_kc_rei_3'
-base_mxd = "bad_river"
-
-print 'set the workspace location...'
-arcpy.env.workspace = os.getcwd()
-fileroot = os.getcwd()
-
-#if os.path.exists(os.path.join(os.getcwd(),"bad_river2.mxd")):
-#	os.remove(os.path.join(os.getcwd(),"bad_river2.mxd"))
-
-print 'open the mxd file'
-### Adding shapefile to the mxd at the top of the TOC
-mxd = arcpy.mapping.MapDocument(r"%s.mxd" %(base_mxd))
-df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
-print 'Add all the layers---->'
-
-infiles = open('arcfiles_' + rootname + '.dat','r').readlines()
-for line in infiles:	
-	filename = os.path.join(os.getcwd(),line.strip())
-	
-	#	
-	# now add only the OVER and UNDER layers and tweak the layer symbology
-	#
-	if 'over' in filename:
-		print 'adding %s' %(filename)
-		newlayer = arcpy.mapping.Layer(filename)
-		arcpy.mapping.AddLayer(df, newlayer,"TOP")		
-		arcpy.ApplySymbologyFromLayer_management(newlayer, 
-		        os.path.join(os.getcwd(),'over.lyr'))
-	elif 'under' in filename:
-		print 'adding %s' %(filename)
-		newlayer = arcpy.mapping.Layer(filename)
-		arcpy.mapping.AddLayer(df, newlayer,"TOP")				
-		arcpy.ApplySymbologyFromLayer_management(newlayer, 
-		        os.path.join(os.getcwd(),'under.lyr'))
-	
-
-print 'saving down a copy to ->\n%s' %(os.path.join(os.getcwd(),rootname + ".mxd"))
-mxd.saveACopy(rootname + ".mxd")
-
-# Refresh things
-arcpy.RefreshActiveView()
-arcpy.RefreshTOC()
-
-print "Files added ... done for now!"
-=======
 import arcpy
 import os
 
@@ -107,4 +57,3 @@ arcpy.RefreshActiveView()
 arcpy.RefreshTOC()
 
 print "Files added ... done for now!"
->>>>>>> arcresid/master
