a7555ce2175043602497d02fa835117ceba78687
Michael Fienen <mnfienen@usgs.gov>
2013-03-22 17:35:49 -0500

    Fixed tab lengths in beopest_launcher.py

--- beopest_launcher.py
+++ beopest_launcher.py
@@ -32,36 +32,36 @@ currdir = os.getcwd() #determine the current working directory for later moving 
 
 # kill the old run directories
 if KILLOLD:
-	for i in range(numworkers):
-		i = i + workerstartnum
-		print 'Removing --> worker%s' %(i)
-		if os.path.exists(os.path.join(currdir,'worker%s' %(i))):
-			shutil.rmtree(os.path.join(currdir,'worker%s' %(i)))
+    for i in range(numworkers):
+        i = i + workerstartnum
+        print 'Removing --> worker%s' %(i)
+        if os.path.exists(os.path.join(currdir,'worker%s' %(i))):
+            shutil.rmtree(os.path.join(currdir,'worker%s' %(i)))
 
 # make the new directories
 if MAKENEW:
-	for i in range(numworkers):
-		i = i + workerstartnum		
-		print 'Making directory ---> worker%s' %(i)
-		os.mkdir('worker%s' %(i))
-		for cfold in mod_dirs:
-			print '     copying %s' %(cfold)
-			shutil.copytree(cfold,os.path.join(currdir,'worker%s' %(i),cfold))
+    for i in range(numworkers):
+        i = i + workerstartnum		
+        print 'Making directory ---> worker%s' %(i)
+        os.mkdir('worker%s' %(i))
+        for cfold in mod_dirs:
+            print '     copying %s' %(cfold)
+            shutil.copytree(cfold,os.path.join(currdir,'worker%s' %(i),cfold))
 
 # launch the master
 if STARTMASTER:
-	os.chdir(os.path.join(currdir,stdir))
-	os.system('start %s %s %s :%d' %(bp,casefile,mflags,portnum))
-	os.chdir(currdir)
+    os.chdir(os.path.join(currdir,stdir))
+    os.system('start %s %s %s :%d' %(bp,casefile,mflags,portnum))
+    os.chdir(currdir)
 
 if STARTWORKERS:
-	print 'pausing 10 seconds to let the master get rolling'
-	for csec in range(10): # wait 10 seconds to be sure the master started up correctly
-		print '. '*(csec+1)
-		time.sleep(1)
-	for i in range(numworkers):
-		i = i + workerstartnum		
-		print 'Starting ---> worker%s' %(i)
-		os.chdir(os.path.join('worker%s' %(i),stdir))
-		os.system('start %s %s %s %s:%d' %(bp,casefile,wflags,masterip,portnum))
-		os.chdir(currdir)
+    print 'pausing 10 seconds to let the master get rolling'
+    for csec in range(10): # wait 10 seconds to be sure the master started up correctly
+        print '. '*(csec+1)
+        time.sleep(1)
+    for i in range(numworkers):
+        i = i + workerstartnum		
+        print 'Starting ---> worker%s' %(i)
+        os.chdir(os.path.join('worker%s' %(i),stdir))
+        os.system('start %s %s %s %s:%d' %(bp,casefile,wflags,masterip,portnum))
+        os.chdir(currdir)
